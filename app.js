4fba82881714246856325ce2741a3b7c1de183cd
Claire :) <cmurray@sandboxunion.com>
2023-04-15 19:07:12 -0400

    add body parser

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 // Import Required Modules
 const http = require('http');
 const express = require('express');
-const router = require('./router')
+const router = require('./router');
+var bodyParser = require('body-parser');
 
 // Initialize Express server
 app = express();
@@ -13,8 +14,18 @@ app = express();
 app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
 
+// body parser setup
+app.use(bodyParser.urlencoded({ 
+    limit: '200mb',
+    extended: true }));
+  app.use(bodyParser.json({
+    limit: '200mb',
+    extended: true
+  }));
+
 // All routes will be relative to '/' and found in router.js
 app.use('/', router);
+
 
 // Start listening on the port
 port = process.env.PORT || 3000;
